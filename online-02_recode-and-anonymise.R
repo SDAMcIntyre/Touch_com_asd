404fd124f94e4f3351e0b21bfd135227afcfaa04
S McIntyre <sdamcintyre+git@gmail.com>
2022-05-05 20:33:12 +0200

    recode complete and data saved

--- online-02_recode-and-anonymise.R
+++ online-02_recode-and-anonymise.R
@@ -29,8 +29,106 @@ valid_data <- read_csv("Data/private/online_valid-data.csv")
 
 #### recode data   #### 
 
+####. independent variables   #### 
+data_indep <- valid_data %>% 
+  
+  # make group variable from ASD question
+  mutate(
+    group = case_when(
+      ASD == "Yes" ~ "ASD",
+      ASD == "No" ~ "Control"
+    ) 
+  ) %>% 
+
+  # make task variable for which comm task they were assigned
+  mutate(
+    task = coalesce(
+      `Communication Condition ASD`,
+      `Communication Condition Control`
+      )
+    ) %>% 
+  select(-starts_with("Communication Condition")) %>% 
+  mutate(
+    task = case_when(
+      str_detect(task, "FL_(40|29)") ~ "forced choice", 
+      str_detect(task, "FL_(36|30)") ~ "free text"
+      )
+    ) %>% 
+  
+  # keep independent variables
+  select(c(
+    PID,
+    group,
+    task
+  ))
+
+
+####. communication data ####
+
+data_comm_trial_order <- valid_data %>% 
+  # make single variable for display order (qualtrics gives separate variables for group and Task)
+  mutate(
+    `Comm Display Order` = coalesce(
+      `CommunicationFC DO ASD`,
+      `CommunicationFT DO ASD`,
+      `CommunicationFC DO Control`,
+      `CommunicationFT DO Control`
+    ) %>% str_remove_all("[a-z]")
+  ) %>% 
+  # get the trial numbers from order of presentation
+  select(PID,`Comm Display Order`) %>% 
+  separate(
+    `Comm Display Order`,
+    into = paste0("o",1:6),
+    sep = "\\|"
+  ) %>% 
+  pivot_longer(
+    cols = starts_with("o"),
+    names_to = "trial",
+    names_prefix = "o",
+    names_transform = list(trial = parse_integer),
+    values_to = "cued"
+  ) %>% 
+  mutate(
+    cued = case_when(
+      cued == "1" ~ "attention",
+      cued == "2" ~ "calming",
+      cued == "3" ~ "gratitude",
+      cued == "4" ~ "happiness",
+      cued == "5" ~ "love",
+      cued == "6" ~ "sadness"
+    )
+  )
+  
+
+data_comm <- valid_data %>% 
+  
+  # re-name FC responses
+  mutate(across(
+    .cols = matches("CommunicationFC .+") & !contains('DO'),
+    .fns = extract_touches
+    )) %>% 
+  
+  # keep comm variables
+  select(
+    PID, 
+    matches("CommunicationF. .+") & !contains('DO')
+    )  %>% 
+  
+  # put into tidy format like live study
+   pivot_longer(
+    cols = contains("F"),
+    names_to = "cued",
+    names_prefix = "CommunicationF. ",
+    names_transform = list(cued = tolower),
+    values_to = "response"
+    ) %>% 
+  
+  # remove NAs due to not being in the condition (FC/FT)
+  na.omit()
+
 ####. qualtrics variables   #### 
-recoded_data_qualtrics <- valid_data %>% 
+data_qualtrics <- valid_data %>% 
   
   # add minutes/hours duration data 
   mutate(
@@ -46,49 +144,49 @@ recoded_data_qualtrics <- valid_data %>%
     `Recorded Date`,
     `Duration (minutes)`,
     `Duration (hours)`
-    ))
+  ))
 
 ####. demographics variables   #### 
-recoded_data_demog <- valid_data %>% 
+data_demog <- valid_data %>% 
   
   # nice language response labels
   mutate(
     Language = case_when(
       `User Language` == "EN" ~ "English",
       `User Language` == "SV" ~ "Swedish"
-      )
-    ) %>% 
+    )
+  ) %>% 
   
   # nice country of residence names
   mutate(
     `Country of Residence` = coalesce(
       `Country of Residence (EN)`, 
       `Country of Residence (SV)`
-      )
-    ) %>% 
+    )
+  ) %>% 
   mutate(
     `Country of Residence` = case_when(
       `Country of Residence` == "Sverige" ~ "Sweden",
       `Country of Residence` == "United Kingdom of Great Britain and Northern Ireland" ~ "UK",
       `Country of Residence` == "United States of America" ~ "USA",
       TRUE ~ `Country of Residence`
-      )
-    ) %>% 
+    )
+  ) %>% 
   
   # collapse age into bins
-mutate(
-  `Age Group` = case_when(
-    between(Age, 16, 20) ~ '16 - 20',
-    between(Age, 21, 25) ~ '21 - 25',
-    between(Age, 26, 30) ~ '26 - 30',
-    between(Age, 31, 35) ~ '31 - 35',
-    between(Age, 36, 40) ~ '36 - 40'
-  ),
-  `Age Cohort` = case_when(
-    between(Age, 16, 17) ~ "Youth (16 - 17)",
-    between(Age, 18, 40) ~ "Adult (18 - 40)"
-  )
-) %>% 
+  mutate(
+    `Age Group` = case_when(
+      between(Age, 16, 20) ~ '16 - 20',
+      between(Age, 21, 25) ~ '21 - 25',
+      between(Age, 26, 30) ~ '26 - 30',
+      between(Age, 31, 35) ~ '31 - 35',
+      between(Age, 36, 40) ~ '36 - 40'
+    ),
+    `Age Cohort` = case_when(
+      between(Age, 16, 17) ~ "Youth (16 - 17)",
+      between(Age, 18, 40) ~ "Adult (18 - 40)"
+    )
+  ) %>% 
   
   # keep demographics variables
   select(
@@ -99,72 +197,9 @@ mutate(
     `Age Cohort`
   )
 
-####. main independent variables   #### 
-recoded_data_indep <- valid_data %>% 
-  
-  # make group variable from ASD question
-  mutate(
-    group = case_when(
-      ASD == "Yes" ~ "ASD",
-      ASD == "No" ~ "Control"
-    ) 
-  ) %>% 
+####. AQ ####
 
-  # make task variable for which comm task they were assigned
-  mutate(
-    `Communication Task` = coalesce(
-      `Communication Condition ASD`,
-      `Communication Condition Control`
-      )
-    ) %>% 
-  select(-starts_with("Communication Condition")) %>% 
-  mutate(
-    `Communication Task` = case_when(
-      str_detect(`Communication Task`, "FL_(40|29)") ~ "forced choice", 
-      str_detect(`Communication Task`, "FL_(36|30)") ~ "free text"
-      )
-    ) %>% 
-  
-  # keep independent variables
-  select(c(
-    PID,
-    group,
-    `Communication Task`
-  ))
-
-
-####. main dependent variables ####
-
-####.. communication forced choice responses ####
-recoded_data_comm <- valid_data %>% 
-  
-  # make single variable for display order (qualtrics gives separate variables for group and Task)
-  mutate(
-    `Communication DO` = coalesce(
-      `CommunicationFC DO ASD`,
-      `CommunicationFT DO ASD`,
-      `CommunicationFC DO Control`,
-      `CommunicationFT DO Control`
-    )
-  ) %>% 
-  
-  # re-name FC responses
-  mutate(across(
-    .cols = matches("CommunicationFC .+") & !contains('DO'),
-    .fns = extract_touches
-    )) %>% 
-  
-  # keep comm variables
-  select(
-    PID, 
-    matches("CommunicationF. .+") & !contains('DO'),
-    `Communication DO`
-    ) 
-
-
-####.. AQ ####
-
-recoded_data_aq <- valid_data %>% 
+data_aq <- valid_data %>% 
   rowwise() %>% 
   mutate(
     AQ_n_missing = sum(is.na(c_across(starts_with("AQ")))),
@@ -183,13 +218,9 @@ recoded_data_aq <- valid_data %>%
     AQ_n_missing
   )
 
-# check missing responses
-full_join(recoded_data_indep, recoded_data_aq) %>% 
-  group_by(group,AQ_n_missing) %>% tally()
+####. BAPQ ####
 
-####.. BAPQ ####
-
-recoded_data_bapq <- valid_data %>% 
+data_bapq <- valid_data %>% 
   rowwise() %>% 
   mutate(
     BAPQ_n_missing = sum(is.na(c_across(starts_with("BAPQ")))),
@@ -264,13 +295,9 @@ recoded_data_bapq <- valid_data %>%
     BAPQ_n_missing
   )
 
-# check missing responses
-full_join(recoded_data_indep, recoded_data_bapq) %>% 
-  group_by(group,BAPQ_n_missing) %>% tally()
+####. STQ ####
 
-####.. STQ ####
-
-recoded_data_stq <- valid_data %>% 
+data_stq <- valid_data %>% 
   rowwise() %>% 
   mutate(
     STQ_n_missing = sum(is.na(c_across(starts_with("STQ")))),
@@ -290,13 +317,9 @@ recoded_data_stq <- valid_data %>%
     STQ_n_missing
   )
 
-# check missing responses
-full_join(recoded_data_indep, recoded_data_stq) %>% 
-  group_by(group,STQ_n_missing) %>% tally()
+####. TAS ####
 
-####.. TAS ####
-
-recoded_data_tas <- valid_data %>% 
+data_tas <- valid_data %>% 
   rowwise() %>% 
   mutate(
     TAS_n_missing = sum(is.na(c_across(starts_with("TAS")))),
@@ -369,122 +392,23 @@ recoded_data_tas <- valid_data %>%
     TAS_n_missing
   ) 
 
-full_join(recoded_data_indep, recoded_data_tas) %>% 
-  group_by(group, TAS_n_missing) %>% tally()
+#### save data ####
 
-#### save individual data (demographics and questionnaires) ####
+####. communication data ####
 
-recoded_data_indep %>% 
-  full_join(recoded_data_qualtrics) %>% 
-  full_join(recoded_data_demog) %>% 
-  full_join(recoded_data_aq) %>% 
-  full_join(recoded_data_bapq) %>% 
-  full_join(recoded_data_stq) %>% 
-  full_join(recoded_data_tas) %>% 
-  write_path_csv("Data/primary/", "online_individual_data.csv")
+data_indep %>% 
+  full_join(data_comm_trial_order) %>% 
+  full_join(data_comm) %>% 
+  write_path_csv("Data/primary/", "online_comm_recoded.csv")
 
+####. individual data (demographics and questionnaires) ####
 
+data_indep %>% 
+  full_join(data_qualtrics) %>% 
+  full_join(data_demog) %>% 
+  full_join(data_aq) %>% 
+  full_join(data_bapq) %>% 
+  full_join(data_stq) %>% 
+  full_join(data_tas) %>% 
+  write_path_csv("Data/primary/", "online_individual_recoded.csv")
 
-#### missing questionnaire responses ####
-data_folder <- "Data/"
-if (!dir.exists(data_folder)) {dir.create(data_folder)}
-recoded_data_tas %>% 
-  select(`Response ID`, group, `User Language`, `Country of Residence`, contains("n_missing")) %>% 
-  rowwise() %>% 
-  mutate(total_n_missing = sum(c_across(contains("n_missing")))) %>% 
-  filter(total_n_missing > 0) %>%
-  select(-total_n_missing) %>% 
-  arrange(group, -across(contains("n_missing"))) %>% 
-  write_csv(paste0(data_folder, "online_missing_questionnaire_responses.csv"))
-
-####  survey date / time data  #### 
-
-min(recoded_data$`Start Date`)
-min(recoded_data$`Recorded Date`)
-
-max(recoded_data$`Start Date`)
-max(recoded_data$`Recorded Date`)
-
-recoded_data %>% 
-  filter(group == "ASD") %>% 
-  pull(`Recorded Date`) %>% max()
-
-recoded_data %>% 
-  mutate(`End Difference` = `Recorded Date` - `End Date`) %>% 
-  ggplot(aes(x = `End Difference`)) + geom_histogram()
-
-open_plot_window(width = 4.6, height = 3.9)
-
-recoded_data %>% 
-  ggplot(aes(x = `Recorded Date`)) + 
-  facet_wrap(~ group, ncol = 1) +
-  geom_histogram() +
-  labs(x = 'Completion Date') +
-  theme_bw() 
-
-figure_folder <- "Figures/"
-if (!dir.exists(figure_folder)) {dir.create(figure_folder)}
-ggsave(paste0(figure_folder,"online_completion-date.png"))
-
-recoded_data  %>% 
-  filter(`Duration (in seconds)` <= 60*60) %>% 
-  ggplot(aes(x = `Duration (in minutes)`)) + geom_histogram()
-
-recoded_data %>% 
-  filter(`Duration (in seconds)` > 60*60) %>% 
-  ggplot(aes(x = `Duration (in hours)`)) + geom_histogram()
-
-####  demographics  #### 
-
-open_plot_window()
-
-recoded_data %>% 
-  ggplot() +
-  facet_wrap(group ~ `Communication Task`) +
-  geom_bar(
-    aes(x = `Age Group`, fill = Gender),
-    stat = "count",
-    alpha = 0.7,
-    colour = 'black'
-    ) +
-  scale_fill_brewer(palette = "Set1") +
-  theme_bw()
-
-figure_folder <- "Figures/"
-if (!dir.exists(figure_folder)) {dir.create(figure_folder)}
-ggsave(paste0(figure_folder,"online_demographics-1.pdf"))
-
-ctable(
-  x = recoded_data$group,
-  y = recoded_data$`Communication Task`
-) %>% view
-
-ctable(
-  x = recoded_data$group,
-  y = recoded_data$`User Language`
-) %>% view
-
-ctable(
-  x = recoded_data$`Communication Task`,
-  y = recoded_data$`User Language`
-) %>% view
-
-ctable(
-  x = recoded_data$group,
-  y = recoded_data$`Country of Residence`
-) %>% view
-
-ctable(
-  x = recoded_data$`Communication Task`,
-  y = recoded_data$`Country of Residence`
-) %>% view
-
-ctable(
-  x = recoded_data$group,
-  y = recoded_data$Gender
-) %>% view
-
-ctable(
-  x = recoded_data$`Communication Task`,
-  y = recoded_data$Gender
-) %>% view
