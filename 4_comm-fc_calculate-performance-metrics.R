511885df19e588f33369fab75b408f12cbc75ce3
flyeweyP <flaviaespositoub@gmail.com>
2022-06-28 11:13:48 +0200

    Flavia & Sarah did this together

--- 4_comm-fc_calculate-performance-metrics.R
+++ 4_comm-fc_calculate-performance-metrics.R
@@ -28,3 +28,16 @@ online_comm_data %>%
   group_by(group) %>% 
   do(calculate_performance_metrics(., cued, response)) %>% 
   write_path_csv("Data/processed/", "online-comm_performance-group.csv")
+
+# combined live and online by trial 1 -6
+live_comm_data <- read_csv('Data/primary/live-comm_collated.csv', col_types = cols()) %>% 
+  mutate(experiment = "felt touch")
+
+online_comm_data <- read_csv('Data/primary/online_comm_recoded.csv') %>% 
+  filter(task == 'forced choice') %>% 
+  select(-task) %>% 
+  mutate(experiment = "viewed touch")
+
+comm_data <- full_join(live_comm_data, online_comm_data)
+
+trial.1data <- comm_data %>% filter(trial <=6)
