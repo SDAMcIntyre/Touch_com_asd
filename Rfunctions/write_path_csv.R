720ca278a17f7ff77d7dd3075aebbdc9b36c1275
S McIntyre <sdamcintyre+git@gmail.com>
2022-12-08 16:46:15 +0100

    call package explicitly

--- Rfunctions/write_path_csv.R
+++ Rfunctions/write_path_csv.R
@@ -1,4 +1,4 @@
 write_path_csv <- function(x, path, filename, ...) {
   if (!dir.exists(path)) {dir.create(path, recursive = TRUE)}    
-  write_csv(x, paste0(path, filename, ...))
+  readr::write_csv(x, paste0(path, filename, ...))
 }
