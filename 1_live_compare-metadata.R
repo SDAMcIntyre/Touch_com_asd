42ca4b5e05f6f053bab3ff326397f0cfab64fdac
S McIntyre <sdamcintyre+git@gmail.com>
2022-12-08 15:04:12 +0100

    update comments

--- 1_live_compare-metadata.R
+++ 1_live_compare-metadata.R
@@ -1,15 +1,20 @@
+# load libraries  ####
 library(readxl)
 library(dplyr)
 library(readr)
 library(stringr)
 library(tidyr)
 
-# source all .R files in the Rfunctions directory
+# source all .R files in the Rfunctions directory ####
 sapply(list.files("Rfunctions", full.names = TRUE), source)
 
+# input path ####
 RAW_DATA_FOLDER <- "~/Library/CloudStorage/OneDrive-Linköpingsuniversitet/projects - in progress/Touch Comm ASD/Data/"
 
-# read metadata from scanned questionnaire files
+# output paths ####
+REPORTS_FOLDER <- "Data/reports/"
+
+# read metadata from scanned questionnaire files ####
 scanned <- read_scanned_filenames(RAW_DATA_FOLDER, "\\.pdf") %>% 
   mutate(status = "scanned") %>% 
   pivot_wider(
@@ -19,24 +24,24 @@ scanned <- read_scanned_filenames(RAW_DATA_FOLDER, "\\.pdf") %>%
   ) %>% 
   select(-group)
 
-# read metadata from the communication task
+# read metadata from the communication task ####
 comm_task <- read_task_filenames(RAW_DATA_FOLDER, 'comm.*data\\.csv') %>% 
   group_by(PID,date_computer) %>% 
   tally() %>% 
   mutate(comm_task = "completed") %>% 
   select(-n) 
 
-# read metadata from the pleasantness task
+# read metadata from the pleasantness task ####
 pleas_task <- read_task_filenames(RAW_DATA_FOLDER, 'pleas.*data\\.csv') %>% 
   group_by(PID,date_computer) %>% 
   tally() %>% 
   mutate(pleas_task = "completed") %>% 
   select(-n) 
 
-# read notes about the experiment
+# read notes about the experiment ####
 notes <- read_tsv(paste0(RAW_DATA_FOLDER, "missing-data-notes.txt"))
 
-# save a report giving an overview of the data set
+# save a report giving an overview of the data set ####
 
 computer_tasks <- full_join(comm_task, pleas_task) 
 
@@ -52,6 +57,4 @@ full_join(computer_tasks, scanned) %>%
   ) %>% 
   select(c("PID", starts_with("date"), everything())) %>% 
   arrange(PID) %>% 
-  write_path_csv("Data/reports/", "live_data-validation.csv")
-
-
+  write_path_csv(REPORTS_FOLDER, "live_data-validation.csv")
