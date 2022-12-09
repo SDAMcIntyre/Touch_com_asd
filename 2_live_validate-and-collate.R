a24e9139c8b95242e25e4b2a31c3ad21a8b9e086
S McIntyre <sdamcintyre+git@gmail.com>
2022-12-09 14:59:22 +0100

    screen and publish live data

--- 2_live_validate-and-collate.R
+++ 2_live_validate-and-collate.R
@@ -1,62 +1,111 @@
+# load libraries  ####
 library(readr)
 library(dplyr)
 library(stringr)
 
-# source all .R files in the Rfunctions directory
+# source all .R files in the Rfunctions directory ####
 sapply(list.files("Rfunctions", full.names = TRUE), source)
 
+# input path ####
 RAW_DATA_FOLDER <- "~/Library/CloudStorage/OneDrive-Linköpingsuniversitet/projects - in progress/Touch Comm ASD/Data/"
 
-save_folder <- 'Data/primary/'
+# output paths ####
+PRIMARY_DATA_FOLDER <- 'Data/primary/'
 
-#
-collated_comm_data <- collate_live_data(RAW_DATA_FOLDER, 'comm.*data\\.csv') 
+# participant exclusions ####
+EXCLUDED_PIDS <- c(
+  "asd19", # exclusion criterion missed in screening
+  "sub15" # no data for either touch task
+)
+
+# ===== MAIN ===== 
+
+# collate communication task data ####
+comm_data <- collate_live_data(RAW_DATA_FOLDER, 'comm.*data\\.csv') %>% 
+  #. remove excluded participants ####
+  filter(!(PID %in% EXCLUDED_PIDS)) 
 
 # expect 60 trials per participant, check for discrepancies
-collated_comm_data %>% 
+comm_data %>% 
   group_by(path) %>% 
   tally %>% 
   filter(n != 60)
 
-collated_comm_data %>% 
+comm_data %>% 
   group_by(PID) %>% 
   tally %>% 
   filter(n != 60)
 
-collated_comm_data %>% 
-  select(-path) %>%
-  write_path_csv(save_folder,'live-comm_collated.csv')
+# asd 13 - the task was stopped part-way through and re-started, so there are two files, and one extra trial was run. 
 
-collated_pleas_data <- RAW_DATA_FOLDER %>% 
-  collate_live_data('pleas.*data\\.csv') 
+# collate pleasantness rating data ####
+pleas_data <- collate_live_data(RAW_DATA_FOLDER, 'pleas.*data\\.csv')  %>% 
+  #. remove excluded participants ####
+  filter(!(PID %in% EXCLUDED_PIDS)) 
 
 # expect 6 trials per participant, check for discrepancies
-collated_pleas_data %>% 
+pleas_data %>% 
   group_by(path) %>% 
   tally %>% 
   filter(n != 6)
 
-collated_pleas_data %>% 
+pleas_data %>% 
   group_by(PID) %>% 
   tally %>% 
   filter(n != 6)
 
-collated_pleas_data %>% 
-  select(-path) %>%
-  write_path_csv(save_folder,'live-pleas_collated.csv')
+# read data manually entered from paper surveys ####
 
-# read data manually entered from paper surveys
-
-live_individual_data <- read_csv(paste0(RAW_DATA_FOLDER, "demographic-data_entered-and-checked.csv")) %>% 
+indiv_data <- read_csv(paste0(RAW_DATA_FOLDER, "demographic-data_entered-and-checked.csv")) %>% 
   full_join(read_csv(paste0(RAW_DATA_FOLDER, "AQ_entered-and-checked.csv"))) %>% 
   full_join(read_csv(paste0(RAW_DATA_FOLDER, "BAPQ_entered-and-checked.csv"))) %>% 
   full_join(read_csv(paste0(RAW_DATA_FOLDER, "STQ_entered-and-checked.csv"))) %>% 
   full_join(read_csv(paste0(RAW_DATA_FOLDER, "TAS_entered-and-checked.csv"))) %>% 
-  full_join(read_csv(paste0(RAW_DATA_FOLDER, "medication-family_entered-and-checked.csv"))) %>% 
   rename("group" = Group)
 
-## ADD VALIDATION HERE ##
+# read in separately, as it contains lots of potentially identifying info
+med_fam_data <- read_csv(paste0(RAW_DATA_FOLDER, "medication-family_entered-and-checked.csv"))
 
-private_data_folder <- "Data/private"
-if (!dir.exists(private_data_folder)) {dir.create(private_data_folder)}
-write_csv(live_individual_data, paste0(private_data_folder,"/live_valid-individual-data.csv"))
+anon_indiv_data <- indiv_data %>% 
+  mutate(
+    # anonymise experimenter ####
+    Experimenter = Experimenter %>% as.factor() %>% as.numeric(),
+    
+    # collapse age into bins ####
+    `Age Group` = case_when(
+      between(Age, 16, 20) ~ '16 - 20',
+      between(Age, 21, 25) ~ '21 - 25',
+      between(Age, 26, 30) ~ '26 - 30',
+      between(Age, 31, 35) ~ '31 - 35',
+      between(Age, 36, 40) ~ '36 - 40'
+    ),
+    `Age Cohort` = case_when(
+      between(Age, 16, 17) ~ "Youth (16 - 17)",
+      between(Age, 18, 40) ~ "Adult (18 - 40)",
+      PID == "asd01" ~ "Adult (18 - 40)" # age data missing for asd01 but files were saved in the "adult" folder
+    )
+  ) %>% 
+  
+  # remove exact age for privacy ####
+  select(-Age)
+
+# quick look at the data
+anon_indiv_data %>% 
+  select(
+    -(starts_with("AQ") | starts_with("BAPQ") | starts_with("STQ") | starts_with("TAS") )
+    ) %>% View
+  
+# save data files ####
+comm_data %>% 
+  #. remove source file name with date information for privacy ####
+select(-path) %>%
+  write_path_csv(PRIMARY_DATA_FOLDER,'live_comm-data.csv')
+
+pleas_data %>% 
+  #. remove source file name with date information for privacy ####
+select(-path) %>%
+  write_path_csv(PRIMARY_DATA_FOLDER,'live_pleas-data.csv')
+
+anon_indiv_data %>% 
+  write_path_csv(PRIMARY_DATA_FOLDER,"live_valid-anon-indiv-data.csv")
+
