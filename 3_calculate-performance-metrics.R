3e87092531cb6354c556d2e39460bb3870b7ea25
S McIntyre <sdamcintyre+git@gmail.com>
2023-05-11 16:07:28 +0200

    fix first 6 trials performance metrics calculation

--- 3_calculate-performance-metrics.R
+++ 3_calculate-performance-metrics.R
@@ -8,7 +8,10 @@ sapply(list.files("Rfunctions", full.names = TRUE), source)
 PROCESSED_DATA_FOLDER <- "Data/processed/"
 
 # read in the data ####
-comm_fc_data <- read_csv(paste0(PROCESSED_DATA_FOLDER,'communication-data.csv'), col_types = "cccccccccc") %>% 
+comm_fc_data <- read_csv(
+  paste0(PROCESSED_DATA_FOLDER,'communication-data.csv'), 
+  col_types = "cccccicc"
+  ) %>% 
   filter(task == 'forced choice') %>% 
   select(-task)
 
@@ -25,12 +28,13 @@ comm_fc_data %>%
 
 comm_fc_data %>% 
   group_by(experiment) %>% 
-  do(calculate_performance_metrics(., cued, response, group)) #%>%  
+  do(calculate_performance_metrics(., cued, response, group)) %>%  
   write_path_csv(PROCESSED_DATA_FOLDER, "comm_performance-group.csv")
 
 ## combined live and online by trial 1 -6 ####
 
 comm_fc_data %>% 
   filter(trial <=6) %>% 
-  do(calculate_performance_metrics(., cued, response, experiment, group)) %>% 
+  group_by(experiment) %>% 
+  do(calculate_performance_metrics(., cued, response, group)) %>% 
   write_path_csv(PROCESSED_DATA_FOLDER, "comm_performance-group-first6.csv")
