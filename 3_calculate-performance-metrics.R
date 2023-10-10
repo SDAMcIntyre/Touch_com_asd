59b027d0529356a7185cd95f593701ad12b902c0
S McIntyre <sdamcintyre+git@gmail.com>
2023-10-10 17:18:48 +0200

    implemented bootstrapped CIs on F1 score

--- 3_calculate-performance-metrics.R
+++ 3_calculate-performance-metrics.R
@@ -1,5 +1,6 @@
 library(readr)
 library(dplyr)
+library(boot)
 
 # source all .R files in the Rfunctions directory ####
 sapply(list.files("Rfunctions", full.names = TRUE), source)
@@ -17,56 +18,21 @@ comm_fc_data <- read_csv(
 
 # calculate performance metrics ####
 
-# bootstrapping
-# https://bookdown.org/compfinezbook/introcompfinr/The-Nonparametric-Bootstrap.html
-# https://search.r-project.org/CRAN/refmans/boot/html/boot.html
-
-x <- comm_fc_data %>% 
-  filter(experiment == "felt touch")
-
-f1(label = "attention", x$cued, x$response)
-f1_chance("attention", x$cued, x$response)
-
-xdata <- tibble(
-  item = x$cued,
-  resp = x$response
-) 
-
-library(boot)
-
-f1_for_boot <- function(xdata, idx, label) {
-  f1(label, xdata[["item"]][idx], xdata[["resp"]][idx])
-}
-
-f1_for_boot(xdata, 1:4081, label = "happiness")
-
-f1_boot <- boot(xdata, f1_for_boot, R = 1000, label = "happiness")
-plot(f1_boot)
-boot.ci(f1_boot, conf = 0.95, type="perc")
-
-comm_fc_data %>% 
-  filter(experiment == "felt touch") %>% 
-  group_by(group) %>% # just to preserve the variable label
-  do(calculate_performance_metrics(., cued, response, PID))
-
 ## by individual, felt only ####
 comm_fc_data %>% 
   filter(experiment == "felt touch") %>% 
-  group_by(group) %>% # just to preserve the variable label
-  do(calculate_performance_metrics(., cued, response, PID)) %>% 
+  calculate_metrics(ORDERED_CUES, "cued", "response", R = 1000, group, PID) %>% 
   write_path_csv(PROCESSED_DATA_FOLDER, "comm-felt-touch_performance-indiv.csv")
 
 ## by group ####
 
 comm_fc_data %>% 
-  group_by(experiment) %>% 
-  do(calculate_performance_metrics(., cued, response, group)) %>%  
+  calculate_metrics(ORDERED_CUES, "cued", "response", R = 1000, experiment, group) %>% 
   write_path_csv(PROCESSED_DATA_FOLDER, "comm_performance-group.csv")
 
 ## combined live and online by trial 1 -6 (first presentation of each touch) ####
 
 comm_fc_data %>% 
   filter(trial <=6) %>% 
-  group_by(experiment) %>% 
-  do(calculate_performance_metrics(., cued, response, group)) %>% 
+  calculate_metrics(ORDERED_CUES, "cued", "response", R = 1000, experiment, group) %>% 
   write_path_csv(PROCESSED_DATA_FOLDER, "comm_performance-group-first6.csv")
--- Rfunctions/calculate_performance_metrics.R
+++ Rfunctions/calculate_performance_metrics.R
@@ -1,65 +1,42 @@
-calculate_performance_metrics <- function(.data, item, response, ...) {
-  .data %>% 
-    # count up responses for all combos of item and response
-    tally_by(..., {{item}}, {{response}}) %>% 
-    # calculate total number of trials/presentations
-    group_by(...) %>% mutate(Total = sum(n)) %>% 
-    # number of times each cue appeared
-    group_by(...,{{item}}) %>% mutate(Present = sum(n)) %>%
-    # number of times each response was made
-    group_by(..., {{response}}) %>% mutate(Selected = sum(n)) %>%
-    # get rid of unneeded rows (we're summarising, one line for each cue)
-    filter({{item}} == {{response}}) %>% rename('Hits' = 'n') %>% # now counts are just hits
-    # calculate performance variables
-    ungroup() %>% 
-    mutate(Misses = Present - Hits,
-           FalseAlarms = Selected - Hits,
-           CorrectRejections = Total - Present - FalseAlarms,
-           Recall = Hits/Present, # p correct when cue present
-           Precision = Hits/Selected, # p correct with this response
-           Precision = if_else(is.na(Precision), 0, Precision), 
-           F1 = 2*( (Precision*Recall) / (Precision+Recall)), 
-           F1 = if_else(is.na(F1), 0, F1),
-           F1chance = 2*( (Present/Total) / ((Present/Total)+1) ) # always give same answer
-    )
+library(dplyr)
+library(boot)
+
+# label: string, the particular class we are currently investigating 
+# true_class: string vector, the true class of the items
+# predicted_class: string vector, the responses indicating how the items were labelled
+
+present <- function(label, true_class) {
+  sum(true_class == label)
 }
 
-# label: string, the particular item we are currently investigating 
-# item: string vector, the items presented in the experiment
-# response: string vector, the responses indicating how the items were labelled
-
-present <- function(label, item) {
-  sum(item == label)
+selected <- function(label, predicted_class) {
+  sum(predicted_class == label)
 }
 
-selected <- function(label, response) {
-  sum(response == label)
+hits <- function(label, true_class, predicted_class) {
+  sum(true_class == predicted_class & true_class == label)
 }
 
-hits <- function(label, item, response) {
-  sum(item == response & item == label)
+misses <- function(label, true_class, predicted_class) {
+  present(label, true_class) - hits(label, true_class, predicted_class)
 }
 
-misses <- function(label, item, response) {
-  present(label, item) - hits(label, item, response)
+false_alarms <- function(label, true_class, predicted_class) {
+  selected(label, predicted_class) - hits(label, true_class, predicted_class)
 }
 
-false_alarms <- function(label, item, response) {
-  selected(label, response) - hits(label, item, response)
+correct_rejections <- function(label, true_class, predicted_class) {
+  length(true_class) - present(label, true_class) - false_alarms(label, true_class, predicted_class)
 }
 
-correct_rejections <- function(label, item, response) {
-  length(item) - present(label, item) - false_alarms(label, item, response)
+recall <- function(label, true_class, predicted_class) {
+  # p correct when this label is the true class
+  hits(label, true_class, predicted_class) / present(label, true_class)
 }
 
-recall <- function(label, item, response) {
-  # p correct when item present
-  hits(label, item, response) / present(label, item)
-}
-
-precision <- function(label, item, response) {
-  # p correct with this response
-  precision <- hits(label, item, response) / selected(label, response) 
+precision <- function(label, true_class, predicted_class) {
+  # p correct when predicting this label as the class
+  precision <- hits(label, true_class, predicted_class) / selected(label, predicted_class) 
   if ( is.na(precision) ) {
     return(0)
   } else {
@@ -67,10 +44,10 @@ precision <- function(label, item, response) {
   }
 }
 
-f1  <- function(label, item, response) {
+f1  <- function(label, true_class, predicted_class) {
   f1 <- 2 * (
-    ( precision(label, item, response) * recall(label, item, response) ) / 
-      (precision(label, item, response) + recall(label, item, response) )
+    ( precision(label, true_class, predicted_class) * recall(label, true_class, predicted_class) ) / 
+      (precision(label, true_class, predicted_class) + recall(label, true_class, predicted_class) )
     )
   if ( is.na(f1) ) {
     return(0) 
@@ -79,10 +56,77 @@ f1  <- function(label, item, response) {
   }
 }
 
-f1_chance <- function(label, item, response) {
+f1_chance <- function(label, true_class, predicted_class) {
   # predicted f1 score if just guessing
   2 * ( 
-    ( present(label, item) / length(item) ) / 
-      ( (present(label, item) / length(item)) + 1 ) 
+    ( present(label, true_class) / length(true_class) ) / 
+      ( (present(label, true_class) / length(true_class)) + 1 ) 
     )
+}
+
+# bootstrapping
+# https://bookdown.org/compfinezbook/introcompfinr/The-Nonparametric-Bootstrap.html
+# https://search.r-project.org/CRAN/refmans/boot/html/boot.html
+
+# f1 "statistic" function to pass to boot
+f1_for_boot <- function(xdata, idx, label) {
+  f1(label, xdata[["true_class"]][idx], xdata[["predicted_class"]][idx])
+}
+
+
+f1_bootstrap_ci <- function(df, true_class, predicted_class, label, R) {
+  xdata <- tibble(
+    true_class = df[[true_class]],
+    predicted_class = df[[predicted_class]]
+  ) 
+  
+  # do bootstrapping on f1
+  f1_boot <- boot(data = xdata, statistic = f1_for_boot, R = R, parallel = "multicore", label = label)
+  # get CIs
+  f1_boot_ci <- boot.ci(f1_boot, conf = 0.95, type="perc")
+  ci_lower <- f1_boot_ci$percent[4]
+  ci_upper <- f1_boot_ci$percent[5]
+  
+  # output
+  tibble(
+    Label = label,
+    F1 = f1_boot$t0,
+    # set CI to 1,1 if error is generated from boot.ci() call: 
+    #   "All values of t are equal to  1 \n Cannot calculate confidence intervals"
+    F1_bCI_lower = if_else(is.null(ci_lower), 1, ci_lower),
+    F1_bCI_upper = if_else(is.null(ci_upper), 1, ci_upper)
+  )
+}
+
+calculate_metrics <- function(df, labels, true_class, predicted_class, R, ...) {
+  out <- tibble()
+  for (label in labels) {
+    
+    f1_bci <- df %>% 
+      group_by(...) %>% 
+      group_modify( ~ f1_bootstrap_ci(.x, true_class, predicted_class, label = label, R = R))
+    
+    other_metrics <- df %>%
+      group_by(...) %>%
+      summarise(
+        Label = label,
+        F1_chance = f1_chance(label, get(!!true_class), get(!!predicted_class)),
+        Present = present(label, get(!!true_class)),
+        Selected = selected(label, get(!!predicted_class)),
+        Hits = hits(label, get(!!true_class), get(!!predicted_class)),
+        Misses = misses(label, get(!!true_class), get(!!predicted_class)),
+        `False Alarms` = false_alarms(label, get(!!true_class), get(!!predicted_class)),
+        `Correct Rejections` = correct_rejections(label, get(!!true_class), get(!!predicted_class)),
+        Recall = recall(label, get(!!true_class), get(!!predicted_class)),
+        Precision = precision(label, get(!!true_class), get(!!predicted_class))
+      )
+    
+    combined <- full_join(f1_bci, other_metrics)
+    
+    out <- rbind(
+      out, 
+      combined
+    )
+  }
+  out %>% select(..., everything()) %>% arrange(...)
 }
