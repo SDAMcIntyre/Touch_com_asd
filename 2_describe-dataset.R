cfe48c4370ed60e25cd3ef11bd422eec4d3bbddb
S McIntyre <sdamcintyre+git@gmail.com>
2023-10-11 18:21:24 +0200

    added group variable to strict dataset file

--- 2_describe-dataset.R
+++ 2_describe-dataset.R
@@ -152,7 +152,7 @@ demog_forced_strict %>%
 #.. save PIDs for strict subsets ####
 
 demog %>% 
-  select(PID, experiment) %>% 
+  select(experiment, group, PID) %>% 
   mutate(
     felt_touch = PID %in% demog_felt_strict$PID,
     viewed_touch = PID %in% demog_viewed_strict$PID,
