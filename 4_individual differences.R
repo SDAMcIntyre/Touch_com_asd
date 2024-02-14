36bd5e9a51bf10dd341811d84700024af1a64efa
S McIntyre <sdamcintyre+git@gmail.com>
2024-02-14 20:54:58 +0100

    complete individual differences analysis

---
+++ 4_individual differences.R
@@ -0,0 +1,233 @@
+library(readr)
+library(dplyr)
+# library(afex)
+library(psych)
+library(ggplot2)
+# library(stringr)
+library(tidyr)
+# library(emmeans)
+library(patchwork)
+library(svglite)
+# source .R files in the Rfunctions directory
+source("Rfunctions/plot_appearance.R")
+source("Rfunctions/open_plot_window.R")
+source("Rfunctions/corr_matrix.R")
+
+# input path ####
+PROCESSED_DATA_FOLDER <- "Data/processed/"
+
+# output paths ####
+FIGURES_FOLDER <- "Figures/"
+if ( !dir.exists(FIGURES_FOLDER) ) { dir.create(FIGURES_FOLDER) }
+
+# read individual performance metrics ####
+
+comm_F1micro <- read_csv(
+  paste0(PROCESSED_DATA_FOLDER, 'comm_F1micro-indiv.csv'), 
+  col_types = cols()
+) 
+
+# read pleasantness data ####
+
+pleasantness <- read_csv(
+  paste0(PROCESSED_DATA_FOLDER, 'pleasantness-data.csv'), 
+  col_types = cols() 
+) %>% 
+  filter(task == "forced choice") %>% 
+  group_by(experiment, group, PID) %>% 
+  summarise(Pleasantness = mean(response, na.rm = TRUE) )
+
+# read questionnaire data ####
+
+questionnaires <- read_csv(
+  paste0(PROCESSED_DATA_FOLDER, 'questionnaire-data.csv'),
+  col_types = cols() 
+) %>% filter(task == "forced choice")
+
+# combine data ####
+
+indiv_data <- full_join(comm_F1micro, questionnaires) %>% 
+  full_join(pleasantness) %>% 
+  mutate(
+    group_mode = recode(
+      paste(group, experiment),
+      `ASD felt touch` = paste0('Felt, Autistic'), 
+      `Control felt touch` = paste0('Felt, Control'), 
+      `ASD viewed touch` = paste0('Viewed, Autistic'), 
+      `Control viewed touch` = paste0('Viewed, Control') 
+    )
+  )
+
+afex::set_sum_contrasts()
+
+# correlation matrices ####
+
+cormat_felt <- indiv_data %>% 
+  filter(experiment == "felt touch") %>% 
+  select(c(F1_micro, Pleasantness, AQ_total, TAS_total, STQ_total)) %>% 
+  corr.test(adjust = "holm")
+
+print(cormat_felt, short = FALSE)
+cormat_felt$p
+
+plot_cormat_felt <- cormat_felt %>% 
+  corr_matrix_df(var_labels = c("Agreem.", "Pleasant.", "AQ", "TAS", "STQ")) %>% 
+  corr_matrix_plot() +
+  labs(title = "Felt touch")
+
+cormat_viewed <- indiv_data %>% 
+  filter(experiment == "viewed touch") %>% 
+  select(c(F1_micro, Pleasantness, AQ_total, TAS_total, STQ_total)) %>% 
+  corr.test(adjust = "holm")
+
+print(cormat_viewed, short = FALSE)
+cormat_viewed$p
+
+plot_cormat_viewed <- cormat_viewed %>% 
+  corr_matrix_df(var_labels = c("Agreem.", "Pleasant.", "AQ", "TAS", "STQ")) %>% 
+  corr_matrix_plot() +
+  labs(title = "Viewed touch")
+
+open_plot_window()
+plot_cormat_felt + plot_cormat_viewed + plot_layout(guides = "collect")
+ggsave(paste0(FIGURES_FOLDER,"correlation_matrices.svg"), width = 7.9, height = 3.8)
+
+
+# Agreement from AQ ####
+
+#. plots ####
+indiv_data %>% 
+  ggplot(mapping = aes(y = F1_micro, x = AQ_total)) +
+  facet_grid(. ~ experiment) +
+  geom_smooth(method = "lm", se = FALSE, colour = "black", size = 0.5) +
+  geom_point(mapping = aes(colour = group_mode, fill = group_mode)) +
+  theme_light(base_size = 16) +
+  exp_group_scales + 
+  theme_nofacetbox +
+  theme(legend.position = "none") +
+  labs(x = "Autism Quotient (AQ)", y = "Message agreement\n(F1 micro)") 
+
+ggsave(paste0(FIGURES_FOLDER,"Agreement_from_AQ.svg"), width = 8, height = 4)
+
+
+#. stats agreement from AQ ####
+# separate for felt and viewed
+
+mdl_AQ_comm_f <- lm(
+  formula = F1_micro ~  AQ_total, 
+  data = indiv_data,
+  subset = experiment == "felt touch"
+)
+
+plot(mdl_AQ_comm_f)  # https://library.virginia.edu/data/articles/diagnostic-plots
+
+(mdl_AQ_comm_f.aov <- car::Anova(mdl_AQ_comm_f))
+# https://cran.r-project.org/web/packages/effectsize/vignettes/anovaES.html
+effectsize::eta_squared(mdl_AQ_comm_f.aov)
+coef(mdl_AQ_comm_f)
+confint(mdl_AQ_comm_f)
+
+mdl_AQ_comm_v <- lm(
+  formula = F1_micro ~  AQ_total, 
+  data = indiv_data,
+  subset = experiment == "viewed touch"
+)
+
+plot(mdl_AQ_comm_v)
+(mdl_AQ_comm_v.aov <- car::Anova(mdl_AQ_comm_v))
+effectsize::eta_squared(mdl_AQ_comm_v.aov)
+
+
+# Pleasantness from AQ, STQ ####
+
+#. plots ####
+plot_aq_pleas <- indiv_data %>% 
+  ggplot(mapping = aes(y = Pleasantness, x = AQ_total)) +
+  #facet_grid(. ~ experiment) +
+  geom_smooth(method = "lm", se = FALSE, colour = "black", size = 0.5) +
+  geom_point(mapping = aes(colour = group_mode, fill = group_mode)) +
+  theme_light(base_size = 16) +
+  exp_group_scales + 
+  theme_nofacetbox +
+  theme(legend.position = "none") +
+  labs(x = "Autism Quotient (AQ)", y = "Pleasantness rating (VAS)") 
+
+plot_stq_pleas <- indiv_data %>% 
+  ggplot(mapping = aes(y = Pleasantness, x = STQ_total)) +
+  #facet_grid(. ~ experiment) +
+  geom_smooth(method = "lm", se = FALSE, colour = "black", size = 0.5) +
+  geom_point(mapping = aes(colour = group_mode, fill = group_mode)) +
+  theme_light(base_size = 16) +
+  exp_group_scales + 
+  theme_nofacetbox +
+  theme(legend.position = "none") +
+  labs(x = "Social Touch Questionnaire (STQ)", y = NULL) 
+
+plot_aq_pleas + plot_stq_pleas
+
+ggsave(paste0(FIGURES_FOLDER,"Pleasantness_AQ_STQ.svg"), width = 8, height = 4)
+
+
+#. stats pleasantness ####
+
+mdl_pleas <- lm(
+  formula = Pleasantness ~ AQ_total + STQ_total, 
+  data = indiv_data
+)
+
+# plot(mdl_pleas) 
+
+(mdl_pleas.aov <- car::Anova(mdl_pleas, type = 2))
+effectsize::eta_squared(mdl_pleas.aov, partial = TRUE)
+coef(mdl_pleas)
+confint(mdl_pleas)
+
+
+# Sensitivity analyses: ####
+
+#. read subsets for sensitivity analyses ####
+
+comm_F1micro_first6 <- read_csv(
+  paste0(PROCESSED_DATA_FOLDER, 'comm_F1micro-indiv_first6.csv'), 
+  col_types = cols()
+) 
+
+comm_F1micro_strict <- read_csv(
+  paste0(PROCESSED_DATA_FOLDER, 'comm_F1micro-indiv_strict.csv'), 
+  col_types = cols()
+)
+
+comm_F1micro_first6_strict <- read_csv(
+  paste0(PROCESSED_DATA_FOLDER, 'comm_F1micro-indiv_first6_strict.csv'), 
+  col_types = cols()
+)
+
+#. first 6 trials only ####
+
+indiv_data_first6 <- full_join(comm_F1micro_first6, questionnaires)
+
+indiv_data_first6 %>% 
+  ggplot(mapping = aes(y = F1_micro, x = BAPQ_total)) +
+  facet_grid(experiment ~ .) +
+  geom_point(mapping = aes(colour = group)) +
+  geom_smooth(method = "lm")
+
+#. strict dataset (similar sex and age demographics, N) ####
+
+indiv_data_strict <- full_join(comm_F1micro_strict, questionnaires)
+
+indiv_data_strict %>% 
+  ggplot(mapping = aes(y = F1_micro, x = BAPQ_total)) +
+  facet_grid(experiment ~ .) +
+  geom_point(mapping = aes(colour = group)) +
+  geom_smooth(method = "lm")
+
+#. first 6 trials in strict dataset ####
+
+indiv_data_first6_strict <- full_join(comm_F1micro_first6_strict, questionnaires)
+
+indiv_data_first6_strict %>% 
+  ggplot(mapping = aes(y = F1_micro, x = BAPQ_total)) +
+  facet_grid(experiment ~ .) +
+  geom_point(mapping = aes(colour = group)) +
+  geom_smooth(method = "lm")
--- Rfunctions/plot_appearance.R
+++ Rfunctions/plot_appearance.R
@@ -13,8 +13,7 @@ COLOUR_GRATITUDE <- "#B30000"
 
 # RColorBrewer::brewer.pal(12, "Paired")
 # RColorBrewer::display.brewer.pal(12, "Paired")
-# library(unikn)
-# seecol(pal = "pal_unikn_pair")
+# unikn::seecol(pal = "pal_unikn_pair")
 
 COLOUR_ASD_FELT <- "#008ECE"
 COLOUR_ASD_VIEWED <- "#59C7EB"
@@ -44,131 +43,63 @@ theme_confmat_legend <- theme(legend.margin=margin(0,0,0,0),
 theme_nofacetbox <- theme(strip.background = element_blank(), 
                           strip.text = element_text(colour = 'black'))
 
+theme_nofacets <- theme_nofacetbox + theme(strip.text.x = element_text(size=0))
+
 theme_touch_comm_asd <- theme_light(base_size = 14) + 
   theme_x45deg + 
-  theme_insidelegend(0.87,0.87)
+  theme_insidelegend(0.84,0.87)
 
-scales_touch_comm_asd <- list(
-  scale_color_manual(values = c(COLOUR_ASD_FELT, COLOUR_CONTROL_FELT, COLOUR_ASD_VIEWED, COLOUR_CONTROL_VIEWED)),
-  scale_fill_manual(values = c(COLOUR_ASD_FELT, COLOUR_CONTROL_FELT, COLOUR_ASD_VIEWED, COLOUR_CONTROL_VIEWED)),
-  scale_x_discrete(label = str_trunc(str_to_title(ORDERED_CUES),3,'right','')) 
+exp_group_scales <- list(
+  scale_color_manual(name = NULL, values = c(COLOUR_ASD_FELT, COLOUR_CONTROL_FELT, COLOUR_ASD_VIEWED, COLOUR_CONTROL_VIEWED)),
+  scale_fill_manual(name = NULL, values = c(COLOUR_ASD_FELT, COLOUR_CONTROL_FELT, COLOUR_ASD_VIEWED, COLOUR_CONTROL_VIEWED))
 )
 
-scales_group_mode <- list(
-  scale_color_manual(values = rep(c(COLOUR_ASD, COLOUR_CONTROL), 2)),
-  scale_fill_manual(values = rep(c(COLOUR_ASD, COLOUR_CONTROL), 2)),
-  scale_alpha_manual(values = rep(c(ALPHA_FELT, ALPHA_VIEWED), each = 2)),
-  scale_linetype_manual(values = rep(c(LINETYPE_FELT, LINETYPE_VIEWED), each = 2)),
-  scale_x_discrete(label = str_trunc(str_to_title(ORDERED_CUES),3,'right','')) 
+viewed_only_scales <- list(
+  scale_color_manual(name = NULL, values = c(COLOUR_ASD_VIEWED, COLOUR_CONTROL_VIEWED)),
+  scale_fill_manual(name = NULL, values = c(COLOUR_ASD_VIEWED, COLOUR_CONTROL_VIEWED))
 )
 
+x_touch_labels <- scale_x_discrete(label = str_trunc(str_to_title(ORDERED_CUES),3,'right','')) 
 
-# pleasantness appearance now orphaned, need to restore
-# +
-#   scale_y_continuous(breaks = c(-10,0,10), limits = c(-10,10), labels = c('unpleasant','','pleasant')) +
-#   labs(y = 'Pleasantness rating (VAS)', x = NULL) +
 
-f1_indiv_plot <- function(df) {
+# questionnaire boxplots by group ####
+
+questionnaire_plot <- function(df, y) {
   df %>% 
-    ggplot(aes(y = F1, x = Label, colour = group, fill = group)) +
-    geom_hline(yintercept = df$F1_chance[1], 
-               colour = HLINE_COLOUR, linetype = HLINE_LINETYPE, size = HLINE_SIZE) +
-    stat_summary(geom = 'errorbar', fun.data = 'mean_cl_boot', 
-                 width = ERRORBAR_WIDTH, size = ERRORBAR_SIZE,
-                 position = DODGE_WIDTH) +
-    stat_summary(geom = "point", fun= "mean",
-                 size = POINT_SIZE, shape = POINT_SHAPE, 
-                 position = DODGE_WIDTH) +
-    scales_touch_comm_asd +
-    theme_touch_comm_asd +
-    scale_y_continuous(limits = c(0,1)) +
-    labs(x = NULL, y = 'Agreement Score (F1)', colour = NULL, fill = NULL) +
-    annotate("text", x = 1.5, y = 0.25, label = 'italic(chance)', parse = TRUE, colour = 'darkgrey')
+    # for plot colours
+    mutate(
+      group_mode = recode(
+        paste(group, experiment),
+        `ASD felt touch` = paste0('Felt, Autistic'), 
+        `Control felt touch` = paste0('Felt, Control'), 
+        `ASD viewed touch` = paste0('Viewed, Autistic'), 
+        `Control viewed touch` = paste0('Viewed, Control') 
+      )
+    ) %>% 
+    ggplot(mapping = aes(x = group_mode, y = {{y}}, fill = group_mode)) +
+    geom_boxplot() +
+    theme_light(base_size = 16) +
+    theme_x45deg + 
+    theme(legend.position = "none") 
 }
 
-
-f1_group_plot <- function(df) {
-  df %>% 
-    ggplot(mapping = aes(y = F1, x = Label, colour = group, fill = group)) +
-    geom_hline(
-      yintercept = df$F1_chance[1], 
-      colour = HLINE_COLOUR, linetype = HLINE_LINETYPE, size = HLINE_SIZE
-    ) +
-    geom_errorbar(
-      mapping = aes(ymin = F1_bCI_lower, ymax = F1_bCI_upper),
-      width = ERRORBAR_WIDTH, size = ERRORBAR_SIZE, position = DODGE_WIDTH
-    ) +
-    geom_point(
-      size = POINT_SIZE, shape = POINT_SHAPE, position = DODGE_WIDTH
-    ) +
-    scales_touch_comm_asd +
-    theme_touch_comm_asd +
-    scale_y_continuous(limits = c(0,1)) +
-    labs(x = NULL, y = 'Agreement Score (F1)', colour = NULL, fill = NULL) +
-    annotate("text", x = 1.5, y = 0.25, label = 'italic(chance)', parse = TRUE, colour = 'darkgrey')
-}
-
-f1_group_mode_plot <- function(df) {
-  df %>% 
-    ggplot(mapping = aes(
-      y = F1, x = Label, 
-      colour = group_mode, fill = group_mode #linetype = group_mode, alpha = group_mode, 
-    )) +
-    geom_hline(
-      yintercept = df$F1_chance[1], 
-      colour = HLINE_COLOUR, linetype = HLINE_LINETYPE, size = HLINE_SIZE
-    ) +
-    geom_errorbar(
-      mapping = aes(ymin = F1_bCI_lower, ymax = F1_bCI_upper),
-      width = ERRORBAR_WIDTH, size = ERRORBAR_SIZE, position = position_dodge(0.6)
-    ) +
-    geom_point(
-      size = POINT_SIZE, shape = POINT_SHAPE, position = position_dodge(0.6)
-    ) +
-    scales_touch_comm_asd +
-    theme_touch_comm_asd +
-    scale_y_continuous(limits = c(0,1)) +
-    labs(x = NULL, y = 'Agreement Score (F1)', colour = NULL, fill = NULL, alpha = NULL, linetype = NULL) +
-    annotate("text", x = 1, y = 0.25, label = 'italic(chance)', parse = TRUE, colour = 'darkgrey')
-}
-
-f1_emm_plot <- function(df) {
-  df %>% 
-    ggplot(aes(y = prob, x = cued, colour = group, fill = group)) +
-    geom_hline(yintercept = df$F1chance[1], 
-               colour = 'grey', linetype = 'dashed', size = 1) +
-    geom_errorbar(aes(ymin = asymp.LCL, ymax = asymp.UCL),
-                  width = 0.4, size = 1.3,
-                  position = position_dodge(0.2)) +
-    geom_point(size = 6, shape = "\u2014", # unicode m-dash for horizontal line
-               position = position_dodge(0.2)) +
-    scale_color_manual(values = c(COLOUR_ASD, COLOUR_CONTROL)) +
-    scale_fill_manual(values = c(COLOUR_ASD, COLOUR_CONTROL)) +
-    scale_x_discrete(label = str_trunc(str_to_title(ORDERED_CUES),3,'right','')) +
-    scale_y_continuous(limits = c(0,1)) +
-    labs(x = NULL, y = 'Agreement Score (F1)', colour = NULL, fill = NULL) +
-    theme_light(base_size = 14) + theme_x45deg + theme_insidelegend(0.85,0.85) +
-    annotate("text", x = 1.5, y = 0.25, label = 'italic(chance)', parse = TRUE, colour = 'darkgrey')
-}
-
-pleasantness_plot <- function(df) {
-  df %>% 
-    ggplot(aes(y = response, x = cued, colour = group, fill = group)) +
-    geom_hline(yintercept = 0, 
-               colour = HLINE_COLOUR, linetype = HLINE_LINETYPE, size = HLINE_SIZE) +
-    stat_summary(geom = 'errorbar', fun.data = 'mean_cl_boot', 
-                 width = ERRORBAR_WIDTH, size = ERRORBAR_SIZE,
-                 position = DODGE_WIDTH) +
-    stat_summary(geom = "point", fun= "mean",
-                 size = POINT_SIZE, shape = POINT_SHAPE, 
-                 position = DODGE_WIDTH) +
-    scales_touch_comm_asd +
-    theme_touch_comm_asd +
-    scale_y_continuous(
-      breaks = c(-10,0,10), 
-      limits = c(-10,10), 
-      labels = c('unpleasant','','pleasant')
-      ) +
-    labs(y = 'Pleasantness rating (VAS)', x = NULL) 
+questionnaire_plot_combined <- function(df, y, ylab) {
+  y_scale <- c(min(df[[y]], na.rm = TRUE), max(df[[y]], na.rm = TRUE)) 
+  
+  forced <- df %>% 
+    filter(task == "forced choice") %>% 
+    questionnaire_plot(.data[[y]]) +
+    exp_group_scales +
+    scale_y_continuous(limits = y_scale) +
+    labs(x = NULL, y = ylab, title = "Forced choice")
+  
+  free <- df %>% 
+    filter(task == "free text") %>% 
+    questionnaire_plot(.data[[y]]) +
+    viewed_only_scales +
+    scale_y_continuous(limits = y_scale) +
+    labs(x = NULL, y = NULL, title = "Free text")
+  
+  forced + free + plot_layout(widths = c(2,1))
 }
 
